4ae09f69b2d677806e71effdc9f09e355e1611ef
elifclk444@gmail.com <elifclk444@gmail.com>
2021-01-24 00:20:32 +0300

    24 Ocak 2020

---
+++ OOP2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Elif Çelik
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "12345";
+            musteri1.Adi = "Elif";
+            musteri1.Soyadi = "Çelik";
+            musteri1.TcNo = "12345678910";
+
+            //Kodlama.io
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "54689";
+            musteri2.SirketAdi = "Kodlama.io";
+            musteri2.VergiNo = "1234567890";
+
+            //Gerçek-Tüzel
+            //SOLID
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+
+
+           
+
+
+        }
+    }
+}
---
+++ OOP3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            IKrediManager tasıtKrediManager = new TasıtKrediManager();
+            IKrediManager konutKrediManager = new KonutKrediManager();
+            ILoggerService fileLogerService = new FileLoggerService();
+            ILoggerService databaseLoggerService = new DatabaseLoggerService();
+
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(konutKrediManager,fileLogerService);
+
+            List<IKrediManager> krediler = new List<IKrediManager>() {ihtiyacKrediManager , tasıtKrediManager};
+
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+
+        }
+    }
+}
